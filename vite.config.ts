196e5239c2c6d07b887e4d50e17b22b04ebc7c23
om prajapati <omvariya71@gmail.com>
2025-11-29 14:12:28 +0530

    fix

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,7 @@ import runtimeErrorOverlay from "@replit/vite-plugin-runtime-error-modal";
 export default defineConfig({
   plugins: [
     react(),
+    base: process.env.VITE_BASE_PATH  || "/Om-Portfolio",
     runtimeErrorOverlay(),
     ...(process.env.NODE_ENV !== "production" &&
     process.env.REPL_ID !== undefined
